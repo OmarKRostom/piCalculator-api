9d865b24f0cb219f209a46479b335b5f2d05c836
Omar K. Rostom <o.rostom.93@gmail.com>
2020-02-26 01:26:41 +0800

    Removed comment typo

--- source/routes/appRouter.js
+++ source/routes/appRouter.js
@@ -7,7 +7,7 @@ import * as piController from '../controllers/PiController'
 let appRouter = express.Router()
 
 /**
- * Define pi Calculator Routes d
+ * Define pi Calculator Routes
  */
 appRouter.get('/pi', piController.getLatestPi)
 appRouter.get('/pi/reset', piController.resetPi)
